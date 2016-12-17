967d3b061daa0f133e81e19031c91ab75894f264
Yuki Mano <ysm@umail.ucsb.edu>
2016-12-17 02:46:04 -0800

    board display

--- main.cpp
+++ main.cpp
@@ -1,9 +1,68 @@
 #include <iostream> 
+#include <cmath> 
 using namespace std;
+
+char rowD[] = {'-', '1', '2', '3', '4', '-'};
+char rowC[] = {'-', '1', '2', '3', '4', '-'};
+char rowB[] = {'-', '1', '2', '3', '4', '-'};
+char rowA[] = {'-', '1', '2', '3', '4', '-'};
+
+char *mainBoard[5]; 
+
+
+void board();
+void display();
+char mark(char player);
 
 int main()
 {
-  cout << "Hello World!" << endl;
+  board();
+  display();
+
+  char player1 = 'X'; 
+  char player2 = 'O'; 
+  char playerTurns;
   
+  int turn = 1; 
+
+  if(turn%2)
+    playerTurns = player1; 
+  else
+    playerTurns = player2; 
+
+  mark(playerTurns);
+
+
   return 0;
+}
+
+char mark(char player){
+
+}
+
+void display(){
+  cout << endl;
+  cout << endl;
+
+  cout << "---|---|---|---|---" << endl;
+  cout << " D | " << rowD[1] << " | " << rowD[2] << " | " << rowD[3] << " | " << rowD[4] << " " << endl;
+  cout << "---|---|---|---|---" << endl;
+  cout << " C | " << rowC[1] << " | " << rowC[2] << " | " << rowC[3] << " | " << rowC[4] << " " << endl;
+  cout << "---|---|---|---|---" << endl;
+  cout << " B | " << rowB[1] << " | " << rowB[2] << " | " << rowB[3] << " | " << rowB[4] << " " << endl;
+  cout << "---|---|---|---|---" << endl;
+  cout << " A | " << rowA[1] << " | " << rowA[2] << " | " << rowA[3] << " | " << rowA[4] << " " << endl;
+  cout << "---|---|---|---|---" << endl;
+
+  cout << endl;
+  cout << endl;
+
+}
+
+void board(){
+  mainBoard[0] = NULL;
+  mainBoard[1] = rowA; 
+  mainBoard[2] = rowB; 
+  mainBoard[3] = rowC;
+  mainBoard[4] = rowD; 
 }
